90ddd0a6fb0e1357bd6af51e8d7b931e96076d77
VinRanggaSr <vrangga1@gmail.com>
2025-11-23 15:05:39 +0700

    feat : ✨Add Migrate tenant & sewa tenant

---
+++ database/migrations/2025_11_23_060958_create_tenant_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tenant', function (Blueprint $table) {
+            $table->id('id_tenant');
+            $table->string('lokasi_tenant')->nullable();
+            $table->decimal('harga_sewa_tenant', 12, 2)->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tenant');
+    }
+};
